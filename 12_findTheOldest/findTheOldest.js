11b1a2b50c5790234b396aec14aeff7c1f4ac3ac
jessjuniper <154564136+jessjuniper@users.noreply.github.com>
2024-07-04 13:47:15 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    let currentYear = new Date().getFullYear();
+    people.forEach(person => {
+        if (person.yearOfDeath !== undefined) {
+            if (person.yearOfDeath - person.yearOfBirth > oldest.yearOfDeath - oldest.yearOfBirth) {
+                oldest = person;
+            }
+        }
+        else {
+            person.yearOfDeath = currentYear;
+            if (person.yearOfDeath - person.yearOfBirth > oldest.yearOfDeath - oldest.yearOfBirth) {
+                oldest = person;
+            }
+        }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
