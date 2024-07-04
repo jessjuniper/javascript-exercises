db092f97bc33a2292f72a151e0712957409aa2d0
jessjuniper <154564136+jessjuniper@users.noreply.github.com>
2024-07-04 12:50:04 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n<0) return 'OOPS';
+    let firstNum = 0;
+    let secondNum = 1;
+    let sum;
+    if (n == firstNum) return firstNum;
+    else if (n == secondNum) return secondNum;
+    else {
+        for (let i=2; i<=n; i++) {
+            sum=firstNum+secondNum;
+            firstNum=secondNum;
+            secondNum=sum;
+        }
+        return secondNum;
+    }
+    
 };
 
 // Do not edit below this line
