85d98d6eba6cc2bca06c87dcd78e9d435041ce7f
jessjuniper <154564136+jessjuniper@users.noreply.github.com>
2024-07-04 12:36:02 -0400

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[\W_]/g, ''); //make lowercase and remove all spaces and punctuation, only  numbers and letters remain
+    let newStr = str.split('').reverse().join(''); //reverse string
+    return str===newStr; //check if same and return true if so or false if not
 };
 
 // Do not edit below this line
