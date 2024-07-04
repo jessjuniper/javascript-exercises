da62c1877bacd4992eff1355281fd70c92c4a2d8
jessjuniper <154564136+jessjuniper@users.noreply.github.com>
2024-07-04 12:16:49 -0400

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (arr.length==0) return 0;
+  else {
+  return arr.reduce((acc,curr) => {
+    return acc + curr;
+  });
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc,curr) => {
+    return acc * curr;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n==0 || n==1) return 1;
+  else {
+    let result = n;
+    while (n > 1) {
+      n--;
+      result*=n;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
